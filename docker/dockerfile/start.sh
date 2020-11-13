3cc745fabc6b5d8221926ef8c56653111eaf403f
13928985360 <532246081@qq.com>
2020-11-13 11:38:45 +0800

    新增docker 构建

---
+++ docker/dockerfile/start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+API_PATH=$API_PATH;
+
+
+if [ -z "$API_PATH" ]; then
+    API_PATH="127.0.0.1:9300";
+fi
+
+
+apiUrl="proxy_pass  http://$API_PATH/rjgf/api;"
+
+sed -i '29c '"$apiUrl"'' /etc/nginx/conf.d/default.conf
+
+nginx -g "daemon off;"
---
+++ docker/start_build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# 调用参数如下  start_build.sh "images_name" "dockerFileName" -->$1 $2
+
+CONTAINER_NAME=$1
+if [ ! $CONTAINER_NAME ]; then
+    echo "contain_name is blank"
+    exit;
+fi
+
+DOCKER_FILE_NAME=$2
+if [ ! DOCKER_FILE_NAME ]; then
+    echo "dockerfile is blank"
+    exit;
+fi
+
+echo "move dist to dockerfile dir"
+rm -rf ./dockerfile/dist
+mv ../dist ./dockerfile
+
+
+echo "step1 delete images and container"
+
+docker-compose -f docker-compose.yml  stop $CONTAINER_NAME
+docker-compose -f docker-compose.yml  rm -f $CONTAINER_NAME
+
+imageid=$(docker images | grep "$CONTAINER_NAME" | awk '{print $3}')
+if [ "$imageid" != "" ]; then
+   docker images | grep "$CONTAINER_NAME" | awk '{print $3}'|xargs docker rmi
+   echo "clear images finish"
+fi
+
+cd dockerfile
+echo "create images"
+docker build -t $CONTAINER_NAME -f $DOCKER_FILE_NAME .
+
+cd ..
+echo "create container"
+docker-compose -f docker-compose.yml  up -d $CONTAINER_NAME
